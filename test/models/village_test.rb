dfae22c25cc6d9be89c4ef5a1fb7d0d8350a2c64
t4traw <t4traw@gmail.com>
2018-08-06 23:53:44 +0900

    Annotated

--- test/models/village_test.rb
+++ test/models/village_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: villages
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  mode       :integer          default(1)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  created_by :integer
+#  villager   :integer          default(1), not null
+#  wolf       :integer          default(1), not null
+#  seer       :integer          default(0), not null
+#  hunter     :integer          default(0), not null
+#  madman     :integer          default(0), not null
+#
+
 require 'test_helper'
 
 class VillageTest < ActiveSupport::TestCase
