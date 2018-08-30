37bc3124354ce59c754d8055daca3fdca540791e
t4traw <t4traw@gmail.com>
2018-08-31 01:43:16 +0900

    Annotated

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Chat < ApplicationRecord
 end
--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ChatTest < ActiveSupport::TestCase
